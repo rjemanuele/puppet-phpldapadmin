8d74a47d1b177aa2c236a3c5899bf969386faec3
Rob Emanuele <rob@emanuele.us>
2025-04-20 21:52:59 -0700

    Modernize the package installation

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,6 @@
 # PRIVATE CLASS: not to be invoked directly
 class phpldapadmin::install {
-
-  package { 'php7.4-cli':            ensure => 'installed' }
-  package { 'php7.4-ldap':           ensure => 'installed' }
-  package { 'libapache2-mod-php7.4': ensure => 'installed' }
-  package { 'php7.4':                ensure => 'installed' }
-  package { 'php7.4-xml':            ensure => 'installed' }
-  package { 'git':                   ensure => 'installed' }
+  $packages = ['php7.4-cli', 'php7.4-ldap', 'libapache2-mod-php7.4',
+    'php7.4', 'php7.4-xml', 'git']
+  ensure_packages($packages)
 }
